dbf7cd9a81674c01375d5c9c0fdf34640570e869
Lhotte <thomas.lhotte@gmail.com>
2016-03-15 12:41:37 +0100

    on retente maggle 222222 tro daaaark

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,6 @@
 
                    LA PAGE D'ACCUEIL
 
-            {!! Form::open(array('url' => '/post, 'method' => 'POST')) !!}
-            {{-- Champs du formulaire --}}
-
-            {!! Form::submit('Envoyer') !!}
-            {!! Form::close() !!}
-
 
         </div>
     </div>
